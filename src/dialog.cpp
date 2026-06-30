26e869e6005c3a700642007e787c443f643ddca6
Svetozar Poljak <svetozarpoljak173@gmail.com>
2026-06-30 11:09:31 +0200

    figure color bugs

--- src/dialog.cpp
+++ src/dialog.cpp
@@ -6,11 +6,18 @@ Dialog::Dialog(QWidget *parent)
       timeWhite(300), timeBlack(300), whiteToMove(true), 
       row(0), col(0)
 {
-    this->setFixedSize(650, 450);
+    const int BOARD_X = 600;
+    const int BOARD_Y = 200;
+    const int BOARD_LEN = 80*8;
+
+    this->setFixedSize(200+BOARD_LEN, 100+BOARD_LEN);
     this->setWindowTitle("Chess Board");
+    // this->setStyleSheet("QDialog { background-color: #2c3e50; }");
 
     // GUI
-    mainLayout = new QHBoxLayout(this);
+    //mainLayout = new QHBoxLayout(this);
+    QWidget *boardContainer = new QWidget(this);
+    boardContainer->setGeometry(BOARD_X, BOARD_Y, BOARD_LEN, BOARD_LEN);
 
     QPushButton *closeButton = new QPushButton("X", this);
     closeButton->setFocusPolicy(Qt::NoFocus);
@@ -25,45 +32,47 @@ Dialog::Dialog(QWidget *parent)
         "  background-color: #ff1a1a;"
         "}"
     );
-    closeButton->setGeometry(750, 10, 40, 30);
+    closeButton->setGeometry(160+BOARD_X +BOARD_LEN, 10, 40, 30);
     connect(closeButton, &QPushButton::clicked, this, [](){
         ::kill(::getpid(), SIGKILL);
     });    
 
-    boardLayout = new QGridLayout();
-    boardLayout->setAlignment(Qt::AlignTop | Qt::AlignLeft);
+    boardLayout = new QGridLayout(boardContainer);
+    boardLayout->setAlignment(Qt::AlignCenter);
     boardLayout->setSpacing(0);
     boardLayout->setContentsMargins(0,0,0,0);
-    mainLayout->addLayout(boardLayout);
+    //mainLayout->addLayout(boardLayout);
 
     guiBoard.resize(2*BOARD_SIZE, std::vector<QLabel*>(2*BOARD_SIZE, nullptr));
     logicBoard.resize(2*BOARD_SIZE, std::vector<QString>(2*BOARD_SIZE, ""));
 
     boardInit();
 
+    const int CLK_X = BOARD_X + BOARD_LEN + 30;
+    const int CLK_Y = BOARD_Y + (BOARD_LEN/2);
 
     Clock = new QLabel(this);
-    Clock->setGeometry(750, 350, 90, 175);
+    Clock->setGeometry(CLK_X, CLK_Y, 70, 85);
     Clock->setStyleSheet(
         "QWidget { "
         "  background-color: #aaaaaa; "
         "}"
     );
     whiteClock = new QLabel(Clock);
-    whiteClock->setGeometry(5, 5, 80, 80);
+    whiteClock->setGeometry(5, 5, 60, 35);
 
     blackClock = new QLabel(Clock);
-    blackClock->setGeometry(5, 90, 80, 80);
+    blackClock->setGeometry(5, 45, 60, 35);
  
     updateClockStyles();
 
     whiteClockLabel = new QLabel(whiteClock);
-    whiteClockLabel->setGeometry(0, 0, 80, 80);
-   // whiteClockLabel->setAlignment(Qt::AlignCenter);
+    whiteClockLabel->setGeometry(0, 0, 60, 35);
+    whiteClockLabel->setAlignment(Qt::AlignCenter);
     
     blackClockLabel = new QLabel(blackClock);
-    blackClockLabel->setGeometry(0, 0, 80, 80);
-   // blackClockLabel->setAlignment(Qt::AlignCenter);
+    blackClockLabel->setGeometry(0, 0, 60, 35);
+    blackClockLabel->setAlignment(Qt::AlignCenter);
 
 
     chessClockTimer = new QTimer(this);
@@ -71,19 +80,23 @@ Dialog::Dialog(QWidget *parent)
             this, &Dialog::update_chess_clocks);
 
     statusLabel = new QLabel(this);
+    statusLabel->setGeometry(CLK_X, CLK_Y + 100, 70, 40);
+    statusLabel->setStyleSheet("color: black; font-weight: bold;");
 
     // dodaj ih u layout, npr:
-    QVBoxLayout *infoLayout = new QVBoxLayout();
-    infoLayout->addWidget(whiteClockLabel);
-    infoLayout->addWidget(blackClockLabel);
-    infoLayout->addWidget(statusLabel);
-    mainLayout->addLayout(infoLayout);
+   // QVBoxLayout *infoLayout = new QVBoxLayout();
+   // infoLayout->addWidget(whiteClockLabel);
+   // infoLayout->addWidget(blackClockLabel);
+    //infoLayout->addWidget(statusLabel);
+   // mainLayout->addLayout(infoLayout);
 
     evalBar = new QProgressBar(this);
-    evalBar->setRange(0, 100);
+//    evalBar->setGeometry()
+    
     evalBar->setValue(50);
     evalBar->setOrientation(Qt::Vertical);
-    mainLayout->addWidget(evalBar);
+    evalBar->setGeometry(BOARD_X-30, BOARD_Y, 30, BOARD_LEN);
+   // mainLayout->addWidget(evalBar);
 
     // Scanner thread
     scanner = new BoardScanner(nullptr);
@@ -116,13 +129,21 @@ Dialog::~Dialog()
 
 void Dialog::on_square_changed(int row, int col, QString field, bool figPickedUp)
 {
+    
+//logicBoard[this->row][this->col] = field;
+   /* QString figure_color;
+    if(field == wP || field == wK || field == wQ || field == wB || field == wN || field == wR){
+        figure_color = "white";
+    }else{
+        figure_color = "black";
+    }
     //if(this->row < 4){
 	    if ((7 - this->row + this->col) % 2 == 0) {
 	        guiBoard[7 - this->row][this->col]->setStyleSheet(
-	            "background-color: #eeeed2; color: #769656; font-weight: bold;  ");
+	            "background-color: #eeeed2; color:" + figure_color + " #769656; font-weight: bold;  ");
 	    } else {
 	        guiBoard[7 - this->row][this->col]->setStyleSheet(
-	            "background-color: #769656; color: #eeeed2; font-weight: bold;  ");
+	            "background-color: #769656; color:" + figure_color + " #eeeed2; font-weight: bold;  ");
 	    }
     //}else{
 /*
@@ -134,14 +155,36 @@ void Dialog::on_square_changed(int row, int col, QString field, bool figPickedUp
 	            "background-color: #769656; color: #eeeed2; font-weight: bold;  ");
 	    }
     }*/
+    refreshField(this->row, this->col);
+    
     this->row = row;
     this->col = col;
     logicBoard[row][col] = field;
+    //QString figure_color;
+  //  if(field == wP || field == wK || field == wQ || field == wB || field == wN || field == wR){
+    //    figure_color = "white";
+   // }else{
+     //   figure_color = "black";
+  //  }
     refreshField(row, col); // reset pa highlight
     //if(row < 4)
        // guiBoard[3 - row][col]->setStyleSheet("background-color: yellow;  ");
    // else
-        guiBoard[7 - row][col]->setStyleSheet("background-color: yellow;  ");
+
+    QString figure_color;
+    if(field == wP || field == wK || field == wQ || field == wB || field == wN || field == wR){
+        figure_color = "white";
+    }else{
+        figure_color = "black";
+    }
+    guiBoard[7 - row][col]->setStyleSheet(
+        "QLabel { "
+            "background-color: yellow; " 
+            "color: " + figure_color + "; " 
+            "font-weight: bold" + 
+            "padding: 0px; margin: 0px; border: 0px;"
+        "}"
+    );
     
     //if (!figPickedUp) {
       //  whiteToMove = !whiteToMove;
@@ -163,27 +206,31 @@ void Dialog::refreshField(int r, int c)
     }else{
         figure_color = "black";
     }
-    
-    guiBoard[7 - r][c]->setStyleSheet("background-color: " + backgroundColor + ";" + "color: " + figure_color + "; font-weight: bold;");
+
+    guiBoard[7 - r][c]->setStyleSheet(
+        "QLabel { "
+            "background-color: " + backgroundColor + ";" + 
+            "color: " + figure_color + "; " + 
+            "font-weight: bold" + 
+            "padding: 0px; margin: 0px; border: 0px;"
+        "}"
+    );
 
     if(figure == wK || figure == bK)
-        figure_style = QString::fromUtf8("\u265A");
-       
-    if(figure == wQ || figure == bQ)
-        figure_style = QString::fromUtf8("\u265B");
-        
-    if(figure == wR || figure == bR)
-        figure_style = QString::fromUtf8("\u265C");
-        
-    if(figure == wB || figure == bB)
-        figure_style = QString::fromUtf8("\u265D");
-       
-    if(figure == wN || figure == bN)
-        figure_style = QString::fromUtf8("\u265E");
-    
-    if(figure == wP || figure == bP)
-        figure_style = QString::fromUtf8("\u265F");
-    
+        figure_style = QString::fromUtf8("\u265A");       
+    else if(figure == wQ || figure == bQ)
+        figure_style = QString::fromUtf8("\u265B");        
+    else if(figure == wR || figure == bR)
+        figure_style = QString::fromUtf8("\u265C");        
+    else if(figure == wB || figure == bB)
+        figure_style = QString::fromUtf8("\u265D");       
+    else if(figure == wN || figure == bN)
+        figure_style = QString::fromUtf8("\u265E");   
+    else if(figure == wP || figure == bP)
+        figure_style = QString::fromUtf8("\u265F");   
+    else    
+        figure_style = "";
+
     guiBoard[7 - r][c]->setText(figure_style);
 }
 
@@ -199,6 +246,8 @@ void Dialog::boardInit()
             field->setMargin(0); 
             field->setIndent(0);
             field->setAlignment(Qt::AlignCenter);
+            
+            field->setStyleSheet("QLabel { padding: 0px; margin: 0px; border: 0px; }");
 
             QFont f;
             f.setPixelSize(56);
@@ -266,22 +315,24 @@ void Dialog::update_chess_clocks()
     int bMin = timeBlack / 60, bSec = timeBlack % 60;
 
     whiteClockLabel->setText(
-        QString("BELI: %1:%2").arg(wMin, 2, 10, QChar('0')).arg(wSec, 2, 10, QChar('0')));
+        QString("%1:%2").arg(wMin, 2, 10, QChar('0')).arg(wSec, 2, 10, QChar('0')));
     blackClockLabel->setText(
-        QString("CRNI: %1:%2").arg(bMin, 2, 10, QChar('0')).arg(bSec, 2, 10, QChar('0')));
+        QString("%1:%2").arg(bMin, 2, 10, QChar('0')).arg(bSec, 2, 10, QChar('0')));
 
-    if (timeWhite == 0 || timeBlack == 0) {
+    if (timeWhite == 0) {
         chessClockTimer->stop();
-        statusLabel->setText("Timeout!");
+        statusLabel->setText("Timeout\n White!");
+    }else if(timeBlack == 0){
+        chessClockTimer->stop();
+        statusLabel->setText("Timeout\n Black!");
     }
-
 }
 
 void Dialog::keyPressEvent(QKeyEvent *event){
     if(event->key() == Qt::Key_Escape){
         ::kill(::getpid(), SIGKILL);
     }else if(event->key() == Qt::Key_Space){
-        whiteToMove != whiteToMove;
+        whiteToMove = !whiteToMove;
         updateClockStyles();
         //update_chess_clocks();
     }else{
@@ -292,8 +343,8 @@ void Dialog::keyPressEvent(QKeyEvent *event){
 void Dialog::updateClockStyles(){
     QString highlightedWhiteStyle = "border: 4px solid #000000; background-color: #ffffff; color: #000000;";
     QString highlightedBlackStyle = "border: 4px solid #ffffff; background-color: #000000; color: #ffffff;";
-    QString defaultWhiteStyle = "border: 1px solid #000000; background-color: #222222; color: #000000;";
-    QString defaultBlackStyle = "border: 1px solid #ffffff; background-color: #888888; color: #ffffff;";
+    QString defaultWhiteStyle = "border: 1px solid #000000; background-color: #888888; color: #000000;";
+    QString defaultBlackStyle = "border: 1px solid #ffffff; background-color: #222222; color: #ffffff;";
 
     if(whiteToMove){
         whiteClock->setStyleSheet(highlightedWhiteStyle);
