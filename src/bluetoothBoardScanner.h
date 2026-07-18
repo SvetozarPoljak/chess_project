1b05f9ac46309b161e5f6b0e3171034b686090a4
SvetozarPoljak <162304303+SvetozarPoljak@users.noreply.github.com>
2026-07-18 17:48:06 +0200

    Add files via upload

---
+++ src/bluetoothBoardScanner.h
@@ -0,0 +1,41 @@
+#ifndef BOARDSCANNER_H
+#define BOARDSCANNER_H
+
+#include <QObject>
+#include <QUuid>
+#include <QByteArray>
+#include <QBluetoothAddress>
+#include <QLowEnergyController>
+#include <QLowEnergyService>
+
+class BoardScanner : public QObject
+{
+    Q_OBJECT
+public:
+    explicit BoardScanner(QObject *parent = nullptr);
+    ~BoardScanner() = default;
+
+    void stop();
+
+public slots:
+    void process();
+
+signals:
+    void boardState(const int *new_state);
+
+private slots:
+    // slot gets asinhronous 8 byte data packet
+    void onCharacteristicChange(const QLowEnergyCharacteristic &c, const QByteArray &value);
+
+private:
+    bool running;
+
+    int old_state[64];
+    int new_state[64];
+
+    QBluetoothAddress m_moduleAddress;
+    QLowEnergyController *m_controller;
+    QLowEnergyService *m_service;
+};
+
+#endif // BOARDSCANNER_H
